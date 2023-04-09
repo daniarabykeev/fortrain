c7ce15183f556369264ca5e030a1a2fadab2b3e7
daniarabykeev <daniarabykeev@gmail.com>
2023-04-09 20:05:46 +0600

    fix: change routes

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AddProductPage from "../pages/AddProductPage";
 import EditProductPage from "../pages/EditProductPage";
+import MainLayouts from "../layouts/MainLayouts";
 
 function MainRoutes() {
   return (
     <div>
       <Routes>
-        <Route>
+        <Route element={<MainLayouts />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/add" element={<AddProductPage />} />
           <Route path="/edit/:id" element={<EditProductPage />} />
