b260bc92e9d7986c5c3f18e9fd98fc92a1562c46
daniarabykeev <daniarabykeev@gmail.com>
2023-04-18 22:57:50 +0600

    fix: change editProductPage

--- src/pages/EditProductPage.jsx
+++ src/pages/EditProductPage.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { productsContext } from "../contexts/ProductsContext";
+import { useNavigate, useParams } from "react-router-dom";
+
+const initialState = {
+  title: "",
+  price: "",
+};
 
 function EditProductPage() {
-  return <div>EditProductPage</div>;
+  const { getOneProduct, oneProduct, editProduct } =
+    useContext(productsContext);
+  const [product, setProduct] = useState(initialState);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getOneProduct(id);
+  }, []);
+  useEffect(() => {
+    if (oneProduct) {
+      setProduct(oneProduct);
+    }
+  }, [oneProduct]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    editProduct(id, product);
+    setProduct(initialState);
+    navigate("/");
+  }
+
+  function handleChange(e) {
+    let obj = {
+      ...product,
+      [e.target.name]: e.target.value,
+    };
+    setProduct(obj);
+  }
+
+  return (
+    <div>
+      <form
+        action=""
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          value={product.title}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="price"
+          name="price"
+          value={product.price}
+          onChange={(e) => {
+            handleChange(e);
+          }}
+        />
+        <button>edit</button>
+      </form>
+    </div>
+  );
 }
 
 export default EditProductPage;
