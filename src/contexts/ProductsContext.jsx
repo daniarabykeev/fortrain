3e48fb7b368022bc21560471e6ff60a93b4b75f6
daniarabykeev <daniarabykeev@gmail.com>
2023-05-01 23:27:40 +0600

    feat: add context

---
+++ src/contexts/ProductsContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useReducer } from "react";
+import { ACTIONS } from "../helpers/consts";
+
+export const productsContext = createContext();
+
+const initialState = {
+  products: [],
+  oneProduct: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.GET_PRODUCTS:
+      return { ...state, products: action.payload };
+    case ACTIONS.GET_ONE_PRODUCT:
+      return { ...state, oneProduct: action.payload };
+    default:
+      return state;
+  }
+}
+
+function ProductsContext({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const value = {
+    products: state.products,
+    oneProduct: state.oneProduct,
+  };
+  return <productsContext value={value}>{children}</productsContext>;
+}
+
+export default ProductsContext;
